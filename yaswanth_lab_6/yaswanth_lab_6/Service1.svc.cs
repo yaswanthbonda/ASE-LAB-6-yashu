8307dc08679efa3319d417ca57eb708dace9de2b
yaswanthbonda <yaswanthbonda@gmail.com>
2015-02-27 18:21:00 -0600

    files

---
+++ yaswanth_lab_6/yaswanth_lab_6/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace yaswanth_lab_6
+{
+    public class Service1 : IService1
+    {
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "/login?userName={userName}")]
+        public String login(String userName)
+        {
+            return "Welcome  " + userName;
+        }
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+
+}
